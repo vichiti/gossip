6c5760f6e83e857fe306995ff2f9dacfb1a34280
vichiti <vichiti369@gmail.com>
2025-02-14 06:47:34 +0000

    new working code from glitch 4

--- main.py
+++ main.py
@@ -63,3 +63,6 @@ async def echo(client, message):
     else:
         await message.reply_text("I received your message: " + message.text)
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))  # Default to 8000 if PORT is not set
+    uvicorn.run(app, host="0.0.0.0", port=port)
